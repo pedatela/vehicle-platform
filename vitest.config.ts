b074f4b84aaa10002e6458a030be7992d8d6677d
pedatela <lucaspedatela@gmail.com>
2026-04-25 17:20:03 -0300

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,8 +7,16 @@ export default defineConfig({
     environment: 'node',
     coverage: {
       reporter: ['text', 'lcov'],
-      include: ['src/**/*.ts'],
-      exclude: ['src/server.ts', 'src/**/index.ts']
+      include: [
+        'src/app/services/vehicles.service.ts',
+        'src/app/http/middlewares/authorize-role.ts'
+      ],
+      thresholds: {
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80
+      }
     }
   }
 });
